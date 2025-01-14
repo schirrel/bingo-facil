9de306a6dd82b43a6a323c219f2337fcc8dbce28
Alan Schio <alanbschio@gmail.com>
2025-01-14 06:52:53 -0800

    Corriginod header

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,9 +2,9 @@
 import React, { FormEvent, useEffect } from "react";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import signout from "@/firebase/auth/signout";
 import { createNewBingo } from "@/firebase/bingo/create";
 import Loading from "@/components/admin/loading";
+import AdminHeader from "@/components/admin/header";
 function Page() {
   const { user } = useAuthContext();
   const router = useRouter();
@@ -27,65 +27,7 @@ function Page() {
   return (
     <div className="min-h-full">
       {loading ? <Loading /> : <></>}
-      <nav className="bg-gray-800">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex h-16 items-center justify-between">
-            <div className="flex items-center">
-              <div className="block">
-                <div className="flex items-baseline space-x-4">
-                  <a
-                    href="#"
-                    className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                    aria-current="page"
-                  >
-                    Novo Bingo
-                  </a>
-                  {/* <a
-                    href="#"
-                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                  >
-                    Meus Bingos
-                  </a> */}
-
-                  <div className="block md:hidden">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        onClick={signout}
-                        className="block px-4 py-2 text-sm text-white"
-                      >
-                        Sair
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-4 flex items-center md:ml-6">
-                <button
-                  type="button"
-                  onClick={signout}
-                  className="block px-4 py-2 text-sm text-white"
-                >
-                  Sair
-                </button>
-              </div>
-            </div>
-            <div className="-mr-2 flex md:hidden">
-              <button
-                type="button"
-                className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="absolute -inset-0.5"></span>
-                <span className="sr-only">Open main menu</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <AdminHeader />
       <header className="bg-white shadow">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">
